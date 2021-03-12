6449d44293b405688b7aefd575f4222cb83a0824
EnkheeFe <80447354+EnkheeFe@users.noreply.github.com>
2021-03-12 00:31:14 -0800

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	cout<<"Micheal Jackson"<<endl;
+	cout<<"Beat It"<<endl;
+	cout<<"They told him"<<endl;
+	cout<<"Don't you ever come around here."<<endl;
+	cout<<"Don't wanna see your face"<<endl;
+	cout<<"You better disappear."<<endl;
+	cout<<"The fire's in their eyes and"<<endl;
+	cout<<"Their words are really clear"<<endl;
+	cout<<"So beat it,just beat it"<<endl;
+	cout<<"You better run,you better"<<endl;
+	cout<<"Do what you can"<<endl;
+	cout<<"Don't wanna see no blood,"<<endl;
+	cout<<"Don't be a macho man"<<endl;
+	cout<<"You wanna be tough,better"<<endl;
+	cout<<"Do what you can"<<endl;
+	cout<<"So beat it,but you wanna"<<endl;
+	cout<<"Be bad"<<endl;
+	cout<<"Just beat it,beat it,"<<endl;
+	cout<<"Beat it,beat it"<<endl;
+	cout<<"No one wants to be defeated"<<endl;
+	cout<<"Showin' how funky strong"<<endl;
+	cout<<"Is your fight"<<endl;
+	cout<<"It doesn't matter who's"<<endl;
+	cout<<"Wrong or right"<<endl;
+	cout<<"Just beat it,beat it (4x)"<<endl;
+	cout<<"They're out to get you,better"<<endl;
+	cout<<"Leave while you can"<<endl;
+	cout<<"Don't wanna be a boy,you"<<endl;
+	cout<<"Wanna be a man"<<endl;
+	cout<<"You wanna stay alive,better"<<endl;
+	cout<<"Do what you can"<<endl;
+	cout<<"So beat it,just beat it"<<endl;
+	cout<<"You have to show them that"<<endl;
+	cout<<"You're really not scared"<<endl;
+	cout<<"You're playin' with your life, this"<<endl;
+	cout<<"Ain't no truth or dare"<<endl;
+	cout<<"They'll kick you, then they beat you"<<endl;
+	cout<<"Then they'll tell you it's fair"<<endl;
+	cout<<"So beat it,but you wanna"<<endl;
+	cout<<"Be bad"<<endl;
+	cout<<"Just beat it,beat it,"<<endl;
+	cout<<"Beat it,beat it"<<endl;
+	cout<<"No one wants to be defeated"<<endl;
+	cout<<"Showin' how funky strong"<<endl;
+	cout<<"Is your fight"<<endl;
+	cout<<"It doesn't matter who's"<<endl;
+	cout<<"Wrong or right"<<endl;
+	cout<<"Just beat it,beat it,beat it"<<endl;
+	cout<<"Beat it,beat it..."<<endl;
+	
+	return 0;
+}
