65928875037f7b3d690ed6d5b60e59c36585aa0e
EnkheeFe <80447354+EnkheeFe@users.noreply.github.com>
2021-03-14 22:37:22 -0700

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int x,y;
+	string X="Enter x:";
+	cout<<X;
+	cin>>x;
+	cout<<"Enter y:";
+	cin>>y;
+	cout<<"x+y="<<x+y<<endl<<"x-y="<<x-y<<endl<<"x*y="<<x*y<<endl;
+	cout<<"x/y="<<x/y;
+	return 0;
+}
