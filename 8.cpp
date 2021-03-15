a732431fe32582efe17ae65052e69048a16bb94d
EnkheeFe <80447354+EnkheeFe@users.noreply.github.com>
2021-03-14 23:30:46 -0700

    Add files via upload

---
+++ 8.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int x;
+	string X="Radius r:";
+	cout<<X;
+	cin>>x;
+	cout<<"Diameter d:"<<2*x<<endl<<"Circumference C:"<<2*x*3.14<<endl;
+	cout<<"Area="<<3.14*x*x;
+	return 0;
+}
