2fa9b52a8314ab0bb7744678d914895f3931715a
EnkheeFe <80447354+EnkheeFe@users.noreply.github.com>
2021-03-14 23:38:40 -0700

    Add files via upload

---
+++ 9.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	double x;
+	string X="Centimeter :";
+	cout<<X;
+	cin>>x;
+	cout<<"Meter :"<<x/100<<endl<<"Kilometer :"<<x/100000;
+	
+	return 0;
+}
