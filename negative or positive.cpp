a558c20b0c0bbf0419aa9720b4e9966bc6cd4148
EnkheeFe <80447354+EnkheeFe@users.noreply.github.com>
2021-03-17 23:40:23 -0700

    Add files via upload

---
+++ negative or positive.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	
+	int a;
+	cin>>a;
+	if(a<0){
+	cout<<a<<" is negative";
+	}else if(a>0){
+	cout<<a<<" is positve";
+	}else if(a==0){
+	cout<<a<<" is zero";
+	}
+	
+	
+	return 0;
+}
