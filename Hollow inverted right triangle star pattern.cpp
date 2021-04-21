ab6dc51ab5680df823b7cc429a9f99dd66dbf95f
EnkheeFe <80447354+EnkheeFe@users.noreply.github.com>
2021-04-21 00:31:45 -0700

    Add files via upload

---
+++ Hollow inverted right triangle star pattern.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+		
+		int size;
+		
+		cin>>size;
+		
+		
+		for(int i=0; i<size; i++){
+			
+			for(int j=i; j<size; j++){
+				
+				if(i==0 ||j==i || j==size-1 ){
+					cout<<"*";
+				}else{
+					cout<<" ";
+				}
+			
+			}
+					cout<<endl;
+			
+			
+		}
+		
+
+		
+	
+	
+	
+	return 0;
+}
+
